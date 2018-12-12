a4dd9c1ce05aa5b9f533cc3368a4820636091ad1
Neil <neil@hashicorp.com>
2018-12-12 11:04:00 -0800

    Don't be as explicit about the paths.

--- quick-start/outputs.tf
+++ quick-start/outputs.tf
@@ -5,15 +5,15 @@ output "quick_jumphost_ssh_string" {
 
 output "consul_ui" {
   description = "Use this link to access the Consul UI."
-  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:8500/ui"
+  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:8500"
 }
 
 output "vault_ui" {
   description = "Use this link to access the Vault UI."
-  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:8200/ui"
+  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:8200"
 }
 
 output "nomad_ui" {
   description = "Use this link to access the Nomad UI."
-  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:4646/ui"
+  value       = "http://${module.hashistack_lb.azurerm_public_ip_address[0]}:4646"
 }
